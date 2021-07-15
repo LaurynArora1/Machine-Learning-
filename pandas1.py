8487ee94f0f40edc7ceff5cffc30c4f6a01ad523
LaurynArora1 <79740749+LaurynArora1@users.noreply.github.com>
2021-07-16 00:35:06 +0530

    Add files via upload

---
+++ pandas1.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+Employee1 = {'number': [1, 2, 3, 4, 5], 'name': ["abby", "jenny", "marc", "nina", "tom"], 'hourly salary': [3,12,14,15,11]}
+table1 = pd.DataFrame(Employee1)
+Employee2 = {'number': [1, 2, 3, 4, 5], 'name': ["abby", "joana", "marc", "nina", "tom"], 'hourly salary': [3,15,14,15,11]}
+table2 = pd.DataFrame(Employee2)
+print(Employee1)
+print(table1)
+print(table1.head(3))
+print(table1.tail(2))
+fusion1 = pd.merge(table1, table2,on='hourly salary') #it will merge everything that is common in two tables
+
+print(fusion1)
+
+
