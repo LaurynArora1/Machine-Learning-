f1cd8f21ce264491d9b5bb9f623f977e72cc5fb4
LaurynArora1 <79740749+LaurynArora1@users.noreply.github.com>
2021-07-20 01:06:17 +0530

    Add files via upload

---
+++ scipy1.py
@@ -0,0 +1,24 @@
+import scipy as sp
+
+# from scipy(the main package) we need to import the sub-packages
+
+from scipy import integrate
+from scipy import cluster
+from scipy import fftpack
+
+# help(integrate)
+# sp.info(fftpack)
+
+from scipy import special
+f1=special.kelvin(15)
+print(f1)
+
+f2=special.xlogy(2,18)
+print(f2)
+
+f3=special.exp10(5)
+print(f3)
+
+f4=special.sindg(30)
+print(f4)
+
